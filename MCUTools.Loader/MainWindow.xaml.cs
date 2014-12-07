cf4bbff00a26a4b09c4f427d13a141dd8f9ad658
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2014-12-07 14:51:13 +0100

    MCUTools.Loader initial version

---
+++ MCUTools.Loader/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using MCUTools.Loader.Properties;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MCUTools.Loader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _timer;
+        private bool _quit;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _quit = false;
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(3);
+            _timer.IsEnabled = false;
+            _timer.Tick += _timer_Tick;
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            _timer.IsEnabled = false;
+            this.Close();
+        }
+
+        private void BtnLaunch_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Settings.Default.NetworkPath)) throw new Exception("Network Path is not set. Use settings to specify network path");
+                string path = System.IO.Path.Combine(Settings.Default.NetworkPath, "MCUTools.exe");
+                bool check = File.Exists(path);
+                if (check)
+                {
+                    var proclist = Process.GetProcesses();
+                    var mcu = from i in proclist where i.ProcessName.ToLower().Contains("mcutools") select i;
+                    if (mcu.Count() > 0)
+                    {
+                        _quit = true;
+                        throw new Exception("McuTools allready runing. Can't start another instance.\r\nApplication will now Exit");
+                    }
+                    else
+                    {
+                        Process p = new Process();
+                        p.StartInfo.FileName = path;
+                        p.StartInfo.WorkingDirectory = Settings.Default.NetworkPath;
+                        p.StartInfo.UseShellExecute = false;
+                        p.Start();
+                        _timer.IsEnabled = true;
+                    }
+                }
+                else throw new Exception("Invalid Path. MCUTools.exe is not found");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An errorr occured:\r\n" + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                if (_quit) this.Close();
+            }
+        }
+
+        private void BtnOptions_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow sw = new SettingsWindow();
+            sw.ShowDialog();
+        }
+    }
+}
---
+++ MCUTools.Loader/SettingsWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MCUTools.Loader.Properties;
+using System.Windows;
+
+namespace MCUTools.Loader
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            McuPath.Text = Settings.Default.NetworkPath;
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.NetworkPath = McuPath.Text;
+            Settings.Default.Save();
+            this.DialogResult = true;
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
