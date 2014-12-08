74fd728539b4b7cfe381e61e552a47b9c5e54e9f
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2014-12-08 12:50:05 +0100

    MCUShell initial commit

---
+++ MCUShell/McuShell.Kernel/Kernel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McuShell.Kernel
+{
+    public static class Kernel
+    {
+        /// <summary>
+        /// Return a long value as a human readable file size
+        /// </summary>
+        /// <param name="value">a long file size</param>
+        /// <returns>A human readable file size string</returns>
+        public static string GetFileSize(long value)
+        {
+            FileSize size = FileSize.Byte;
+            double outp = 0.0;
+            if (value > (long)FileSize.PetaByte)
+            {
+                outp = value / (double)FileSize.PetaByte;
+                size = FileSize.PetaByte;
+            }
+            else if (value > (long)FileSize.TerraByte)
+            {
+                outp = value / (double)FileSize.TerraByte;
+                size = FileSize.TerraByte;
+            }
+            else if (value > (long)FileSize.GigaByte)
+            {
+                outp = value / (double)FileSize.GigaByte;
+                size = FileSize.GigaByte;
+            }
+            else if (value > (long)FileSize.MegaByte)
+            {
+                outp = value / (double)FileSize.MegaByte;
+                size = FileSize.MegaByte;
+            }
+            else if (value > (long)FileSize.KiloByte)
+            {
+                outp = value / (double)FileSize.KiloByte;
+                size = FileSize.KiloByte;
+            }
+            else
+            {
+                outp = value;
+                size = FileSize.Byte;
+            }
+            return string.Format("{0:0.000} {1}", outp, size);
+        }
+    }
+}
---
+++ MCUShell/McuShell.Kernel/Types.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace McuShell.Kernel
+{
+    /// <summary>
+    /// File Size Constants
+    /// </summary>
+    public enum FileSize: long
+    {
+        /// <summary>
+        /// 1 PetaByte
+        /// </summary>
+        [Description("PiB")]
+        PetaByte = 1125899906842624,
+        /// <summary>
+        /// 1 TerraByte
+        /// </summary>
+        [Description("TiB")]
+        TerraByte = 1073741824,
+        /// <summary>
+        /// 1 GigaByte
+        /// </summary>
+        [Description("GiB")]
+        GigaByte = 1073741824,
+        /// <summary>
+        /// 1 MegaByte
+        /// </summary>
+        [Description("MiB")]
+        MegaByte = 1048576,
+        /// <summary>
+        /// 1 KiloByte
+        /// </summary>
+        [Description("KiB")]
+        KiloByte = 1024,
+        /// <summary>
+        /// 1 Byte
+        /// </summary>
+        [Description("Byte")]
+        Byte = 1
+    }
+}
